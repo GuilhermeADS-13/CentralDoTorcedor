5fcfa7e8c809233464f99b856319300386e9b509
Guilherme <vianna.guilherme2020@gmail.com>
2026-05-19 17:10:37 -0300

    Atualização do projeto

--- reset.php
+++ reset.php
@@ -27,6 +27,7 @@ if(!isset($_GET['token']))
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site | GA</title>
+    <link rel="stylesheet" href="css/reset.css">
 </head>
 <body>
     <div class="container">
